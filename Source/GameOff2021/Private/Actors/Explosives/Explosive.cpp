d981d4505a92476d4588d450121404b38dd928a1
itshb <jesperhellemar@hotmail.com>
2021-11-04 02:35:03 +0100

    Implemented base Explosive actor

--- Source/GameOff2021/Private/Actors/Explosives/Explosive.cpp
+++ Source/GameOff2021/Private/Actors/Explosives/Explosive.cpp
@@ -2,17 +2,70 @@
 
 
 #include "Actors/Explosives/Explosive.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Interfaces/DamageableInterface.h"
 
 AExplosive::AExplosive() {
 	PrimaryActorTick.bCanEverTick = true;
-}
 
-void AExplosive::BeginPlay() {
-	Super::BeginPlay();
-	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = Mesh;
+	Mesh->SetCollisionProfileName("BlockAllDynamic");
+	Mesh->bCastDynamicShadow = true;
+	Mesh->bAffectDynamicIndirectLighting = true;
+	Mesh->SetGenerateOverlapEvents(false);
+	Mesh->SetCanEverAffectNavigation(false);
+
+	ExplosionCollider = CreateDefaultSubobject<USphereComponent>(TEXT("Explosion Collider"));
+	ExplosionCollider->SetupAttachment(RootComponent);
+	ExplosionCollider->SetCollisionProfileName("OverlapAllDynamic");
+	ExplosionCollider->SetGenerateOverlapEvents(false);
+	ExplosionCollider->SetCanEverAffectNavigation(false);
+	ExplosionCollider->OnComponentBeginOverlap.AddDynamic(this, &AExplosive::OnOverlapBegin);
 }
 
 void AExplosive::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
+
+	if(bExploding) {
+		if(ExplosionLifeTime == 0) bExploding = false;
+		else {
+			Elapsed += DeltaTime;
+			Progress = Elapsed / ExplosionLifeTime;
+			if(Progress >= 1.0f) {
+				Progress = 0.0f;
+				Elapsed = 0.0f;
+				bExploding = false;
+			}
+		}
+	}
 }
 
+void AExplosive::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
+	if(bExploding) {
+		const auto Damageable = Cast<IDamageableInterface>(OtherActor);
+		if(Damageable) {
+			Damageable->NotifyDamage(this, OtherActor, Damage);
+		}
+	}
+}
+
+void AExplosive::Explode() {
+	DoExplode();
+}
+
+void AExplosive::DoExplode() {
+	ExplosionCollider->SetGenerateOverlapEvents(true);
+	bExploding = true;
+
+	if(ExplosionVFX->IsValid()) {
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ExplosionVFX, GetActorLocation(), FRotator(0.0f, 0.0f, 0.0f)); // Trust in bAutoDestroy we do ..for now
+	}
+}
+
+float AExplosive::GetRadius() const {
+	return ExplosionCollider->GetScaledSphereRadius();
+}
--- Source/GameOff2021/Public/Actors/Explosives/Explosive.h
+++ Source/GameOff2021/Public/Actors/Explosives/Explosive.h
@@ -6,7 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "Explosive.generated.h"
 
-UCLASS(Abstract)
+UCLASS()
 class GAMEOFF2021_API AExplosive : public AActor {
 	GENERATED_BODY()
 
@@ -15,12 +15,57 @@ class GAMEOFF2021_API AExplosive : public AActor {
 	 * Override for Gas/Effect function
 	 * Interface/Component for Throwable
 	 */
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Mesh, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* Mesh; // Static until need of Skeletal
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Mesh, meta = (AllowPrivateAccess = "true"))
+	class USphereComponent* ExplosionCollider;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = VFX, meta = (AllowPrivateAccess = "true"))
+	class UNiagaraSystem* ExplosionVFX;
+
+	class UNiagaraComponent* VFXActor;
+
+	bool bExploding;
+
+	float Elapsed;
+
+	float Progress;
+
+	void DoExplode();
 	
 protected:
-	virtual void BeginPlay() override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Settings, meta = (ClampMin = "0"))
+	int32 Damage;
+
+	/** 
+	 * How long does the explosion last & deal damage
+	 * zero for instant destruction only
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Settings, meta = (ClampMin = "0.0"))
+	float ExplosionLifeTime;
 	
 	virtual void Tick(float DeltaTime) override;
 
+	virtual void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 public:
 	AExplosive();
+
+	virtual void Explode();
+
+	class UStaticMeshComponent* GetMesh() const { return Mesh; }
+
+	int32 GetDamage() const { return Damage; }
+
+	float GetRadius() const;
+
+	bool IsExploding() const { return bExploding; }
+
+	float GetLifeTime() const { return ExplosionLifeTime; }
+
+	float GetTimeLeft() const { return Progress; }
+
+	class UNiagaraSystem* GetExplosionVFX() const { return ExplosionVFX; }
 };
